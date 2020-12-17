d4360b93d82cad6dcaf2755a0de7d81171c7b15a
Timotej Zatko <timi.zatko@gmail.com>
2020-12-17 22:34:32 +0100

    Fix download data script

--- scripts/download_saliences_and_segmentations_v2.sh
+++ scripts/download_saliences_and_segmentations_v2.sh
@@ -3,4 +3,4 @@
 mkdir -p tmp
 cd tmp || exit
 gdown https://drive.google.com/uc?id=1wrrxVBYTZgg8PC8-ITREP7EZHLeSHC9v --output saliences_and_segmentations_v2.zip
-unzip saliences_and_segmentations_v2s.zip
+unzip saliences_and_segmentations_v2.zip
